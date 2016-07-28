c3832c74ed3b941b27737f6b64238e67e9606228
vasilty <vasilty@gmail.com>
2016-07-28 09:48:57 +0200

    Updated setUp method in tests

--- menu/tests.py
+++ menu/tests.py
@@ -87,50 +87,7 @@ class ViewTests(TestCase):
 
 class FormViewTest(WebTest):
     def setUp(self):
-        self.user1 = User.objects.create_user(
-            username='vasilty',
-            email='vasilty@example.com',
-            password='password',
-        )
-
-        self.ingredient1 = models.Ingredient.objects.create(name='Chocolate')
-        self.ingredient2 = models.Ingredient.objects.create(name='Coconut')
-        self.ingredient3 = models.Ingredient.objects.create(name='Vanilla')
-
-        self.item1 = models.Item.objects.create(
-            name='Chocolate-Vanilla',
-            description='The best Chocolate-Vanilla dessert in the world.',
-            chef=self.user1,
-        )
-        self.item2 = models.Item.objects.create(
-            name='Coconut-Vanilla',
-            description='You will never forget our Coconut-Vanilla '
-                        'masterpiece.',
-            chef=self.user1,
-        )
-
-        self.menu1 = models.Menu.objects.create(
-            season='July2016',
-        )
-
-        self.menu2 = models.Menu.objects.create(
-            season='August2016',
-            expiration_date=datetime.date(2016, 9, 1)
-        )
-
-        self.item1.ingredients = [self.ingredient1, self.ingredient3]
-        self.item1.items = [self.menu1, self.menu2]
-        self.item1.save()
-
-        self.menu1.items = [self.item1]
-        self.menu1.save()
-
-        self.item2.ingredients = [self.ingredient2, self.ingredient3]
-        self.item2.items = [self.menu2]
-        self.item2.save()
-
-        self.menu2.items = [self.item1, self.item2]
-        self.menu2.save()
+        ViewTests.setUp(self)
 
     def test_view_page(self):
         page = self.app.get(reverse('menu:new'))
@@ -141,7 +98,7 @@ class FormViewTest(WebTest):
         page = page.form.submit()
         self.assertContains(page, "This field is required.")
 
-    def test_new_from_success(self):
+    def test_new_form_success(self):
         page = self.app.get(reverse('menu:new'))
         page.form['season'] = 'May2020'
         page.form['items'] = [1, 2]
